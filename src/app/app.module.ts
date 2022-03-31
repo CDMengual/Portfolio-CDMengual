4a962b816048fc0ddf5cacd0a7216600263a1ecc
CDMengual <cdmengual@gmail.com>
2022-03-31 19:43:41 -0300

    Integracion Boostrap y Creacion Acerca de

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SocialBarComponent } from './social-bar/social-bar.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SocialBarComponent
+    SocialBarComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
